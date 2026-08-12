93d29db75a1d0d17b66f86e872c4635c45fbdf5c
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-08-12 10:04:40 +0530

    added FactorialOrder.java

---
+++ FactorialOrder.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class FactorialOrder {
+    public static ArrayList<Integer> primeFac(int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+
+            if (n % i == 0) {
+                result.add(i);
+
+                while (n % i == 0) {
+                    n = n / i;
+                }
+            }
+        }
+
+        return result;
+        
+    }
+}

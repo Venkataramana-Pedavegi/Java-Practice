2891cffca2f6ae1beb4bd4e1c2c1fcf2e1adfc84
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-08-21 17:32:01 +0530

    added ParallelSortArray1 program

---
+++ ParallelSortArray1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ParallelSortArray1 {
+    public static void main(String[] args) {
+        int [] a={40,30,60,70,10};
+        Arrays.parallelSort(a);
+        System.out.println(a);
+    }
+    
+}

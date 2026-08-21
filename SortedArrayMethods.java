8b5a68ce508b00b7875c092d6dd6e32d382cc7f0
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-08-21 17:08:19 +0530

    added SortedArrayMethods program

---
+++ SortedArrayMethods.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public class SortedArrayMethods {
+    public static void main(String[] args) {
+        int[] arr={10,20,50,70};
+        int result=Arrays.binarySearch( arr,50);
+        System.out.println(result);
+
+        System.out.println(Arrays.toString(arr));
+        //System.out.println(result);
+
+    }
+    
+}

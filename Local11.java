fb8b41fc3ce5463fabe3f60bff18163443c26dc1
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-08-31 20:14:56 +0530

    added Local11 program

---
+++ Local11.java
@@ -0,0 +1,9 @@
+public class Local11 {
+    public static void main(String[] args) {
+        String name="Ramana";
+        int age=19;
+        System.out.println("Name:" +name);
+        System.out.println("Age:" +age);
+    }
+    
+}

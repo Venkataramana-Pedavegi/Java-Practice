9a687b3277682869f9e2263d4ac066386bca6772
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-08-31 20:11:59 +0530

    added Static11 program

---
+++ Static11.java
@@ -0,0 +1,10 @@
+public class Static11 {
+    static String name = "ramaa";
+    static int age=19;
+    public static void main(String[] args) {
+        System.out.println("name:" +Static11.name);
+        System.out.println("Age:" +Static11.age);
+
+    }
+    
+}

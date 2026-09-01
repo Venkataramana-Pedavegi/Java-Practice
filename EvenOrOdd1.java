041ed57c9c067a38e8b7e31606d70709b08bde96
24A81A0643 <venkataramanapedavegi9@gmail.com>
2026-09-01 05:43:35 +0530

    added EvenOrOdd1 program

---
+++ EvenOrOdd1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EvenOrOdd1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println("number is even");
+        }else{
+            System.out.println("number is odd");
+        }
+        sc.close();
+    }
+    
+}
